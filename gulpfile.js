a0fc37f929509bf6eaaf95066875f70ea99ce31d
Popoto <wandersonalvesferreira2012@gmail.com>
2016-04-15 00:06:35 -0300

    Automate build to production

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-var gulp  = require('gulp'),
+var gulp = require('gulp'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     concatCss = require('gulp-concat-css'),
     copy = require('gulp-copy2'),
     runSequence = require('run-sequence'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    htmlreplace = require('gulp-html-replace');
 
 // create a default task and just log a message
-gulp.task('default', function() {
+gulp.task('default', function () {
     return gutil.log('Gulp is running!')
 });
 // build js files uglifying and concating then
-gulp.task('build-js', function() {
+gulp.task('build-js', function () {
     return gulp.src([
                     'bower_components/PACE/pace.min.js',
                     'bower_components/angular/angular.js',
@@ -28,7 +29,7 @@ gulp.task('build-js', function() {
 });
 
 gulp.task('build-css', function () {
-    return gulp.src([                    
+    return gulp.src([
                     'res/css/base.css',
                     'res/css/fixed-navigation-bar.css',
                     'res/css/load-screen.css',
@@ -41,30 +42,47 @@ gulp.task('build-css', function () {
 
 gulp.task('copy', function () {
     var paths = [
-        {src:'res/css/mCSB_buttons.png', dest: 'public/'},
-        {src:'res/assets/**', dest: 'public/res/assets/'},
-        {src:'res/views/**', dest: 'public/res/views/'}
+        {
+            src: 'res/css/mCSB_buttons.png',
+            dest: 'public/'
+        },
+        {
+            src: 'res/assets/**',
+            dest: 'public/res/assets/'
+        },
+        {
+            src: 'res/views/**',
+            dest: 'public/res/views/'
+        }
     ];
     return copy(paths);
 });
 
-gulp.task('build', function() {
+gulp.task('html-replace', function () {
+    gulp.src('index.html')
+        .pipe(htmlreplace({
+            'css': 'build.css',
+            'js': 'build.js'
+        })).pipe(gulp.dest('public/'));
+
+});
+
+gulp.task('build', function () {
     var callback = function () {
         gutil.log("Remember to run 'firebase deploy' on your terminal!");
     };
-    runSequence('build-js', 'build-css', 'copy', callback);
-    
+    runSequence('build-js', 'build-css', 'copy', 'html-replace', callback);
+
 });
 
-gulp.task('server', function() {
+
+gulp.task('server', function () {
     browserSync.init({
         server: {
             baseDir: "./"
         },
         port: 8080
     });
-    
+
     gulp.watch(["./res/js/*.js", "./res/css/*.css", "./res/views/*.html"]).on('change', browserSync.reload);
 });
-
-
